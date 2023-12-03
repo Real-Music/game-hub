17be1a8bc11dd8266c80a58b49eb59b390694779
real-music <fedjioraymond@gmail.com>
2023-12-03 13:54:14 +0100

    Get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,7 +1,8 @@
-import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CreditScore from "./CreditScore";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Porps {
   game: Game;
@@ -10,7 +11,7 @@ interface Porps {
 function GameCard({ game }: Porps) {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
