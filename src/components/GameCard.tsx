e8e03d4b6695935930078900868ae8aa215e8bf3
real-music <fedjioraymond@gmail.com>
2023-12-03 13:46:11 +0100

    Displaying credit scroe

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
+import CreditScore from "./CreditScore";
 
 interface Porps {
   game: Game;
@@ -12,9 +13,12 @@ function GameCard({ game }: Porps) {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
-        <PlatformIconList
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
+        <HStack justifyContent="space-between">
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CreditScore score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   );
