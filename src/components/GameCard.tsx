8896d036b48b60272105cb1fbd36ac5279debf52
real-music <fedjioraymond@gmail.com>
2023-12-04 13:38:31 +0100

    Refactor: remove duplicated styles

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,7 +10,7 @@ interface Porps {
 
 function GameCard({ game }: Porps) {
   return (
-    <Card width="300px" borderRadius={10} overflow="hidden">
+    <Card>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
