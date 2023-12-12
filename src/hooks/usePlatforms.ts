535dec886098b2315075607abe5bcfdd08556ba8
real-music <fedjioraymond@gmail.com>
2023-12-12 11:24:26 +0100

    Refactor: fetch games, platforms & genres with react-query

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,14 +1,18 @@
-// import useData from "./useData";
-
+import { useQuery } from "@tanstack/react-query";
 import platform from "../data/platform";
+import apiClient, { FetchResponse } from "../services/api-client";
 
-// interface Platform {
-// 	id: number;
-// 	name: string;
-// 	slug: string;
-// }
+interface Platform {
+	id: number;
+	name: string;
+	slug: string;
+}
 
-// const usePlatform = () => useData<Platform>('/platforms/lists/parents');
-const usePlatform = () => ({ data: platform, isLoading: false, error: null })
+const usePlatform = () => useQuery({
+	queryKey: ['platforms'],
+	queryFn: () => apiClient.get<FetchResponse<Platform>>('/platforms/lists/parents').then(res => res.data),
+	staleTime: 24 * 60 * 60 * 1000, // 24h,
+	initialData: { count: platform.length, results: platform }
+})
 
 export default usePlatform
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+	count: number;
+	results: T[]
+}
+
 export default axios.create({
 	baseURL: "https://api.rawg.io/api",
 	params: { key: 'a5b7a0e99b0e4240ae3a30b2d196a697' }
