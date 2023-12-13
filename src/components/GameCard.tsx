5fc5dc5873df8762dcc2816f7165be157f7c6bc8
real-music <fedjioraymond@gmail.com>
2023-12-13 13:06:06 +0100

    Refactor entities interfaces

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,6 @@
 import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import { Game } from "../entities/Game";
+import Game from "../entities/Game";
 import getCroppedImageUrl from "../services/image-url";
 import CreditScore from "./CreditScore";
 import Emoji from "./Emoji";
--- src/services/game-service.ts
+++
@@ -1,6 +0,0 @@
-import { Game } from "../entities/Game";
-import APIClient from "./api-client";
-
-
-const gameService = new APIClient<Game>('/games');
-export default gameService;
